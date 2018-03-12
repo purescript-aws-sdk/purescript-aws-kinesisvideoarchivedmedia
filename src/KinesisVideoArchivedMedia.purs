ecc43396991e0c5b9b0b9800012700e6f4ca37de
Philippe Vinchon <pvinchon@gilt.com>
2018-03-12 18:04:23 +0000

    PureScript mapping for aws-sdk-js 2.207.0

--- src/KinesisVideoArchivedMedia.purs
+++ src/KinesisVideoArchivedMedia.purs
@@ -20,17 +20,19 @@ import Data.StrMap as StrMap
 import AWS.Request as Request
 import AWS.Request.Types as Types
 
-serviceName = "KinesisVideoArchivedMedia" :: String
-
 
 -- | <p>Gets media for a list of fragments (specified by fragment number) from the archived data in a Kinesis video stream.</p> <note> <p>This operation is only available for the AWS SDK for Java. It is not supported in AWS SDKs for other languages.</p> </note> <p>The following limits apply when using the <code>GetMediaForFragmentList</code> API:</p> <ul> <li> <p>A client can call <code>GetMediaForFragmentList</code> up to five times per second per stream. </p> </li> <li> <p>Kinesis Video Streams sends media data at a rate of up to 25 megabytes per second (or 200 megabits per second) during a <code>GetMediaForFragmentList</code> session. </p> </li> </ul>
 getMediaForFragmentList :: forall eff. GetMediaForFragmentListInput -> Aff (exception :: EXCEPTION | eff) GetMediaForFragmentListOutput
-getMediaForFragmentList = Request.request serviceName "getMediaForFragmentList" 
+getMediaForFragmentList = Request.request service method  where
+    service = Request.ServiceName "KinesisVideoArchivedMedia"
+    method = Request.MethodName "getMediaForFragmentList"
 
 
 -- | <p>Returns a list of <a>Fragment</a> objects from the specified stream and start location within the archived data.</p>
 listFragments :: forall eff. ListFragmentsInput -> Aff (exception :: EXCEPTION | eff) ListFragmentsOutput
-listFragments = Request.request serviceName "listFragments" 
+listFragments = Request.request service method  where
+    service = Request.ServiceName "KinesisVideoArchivedMedia"
+    method = Request.MethodName "listFragments"
 
 
 -- | <p>Kinesis Video Streams has throttled the request because you have exceeded the limit of allowed client calls. Try making the call later.</p>
